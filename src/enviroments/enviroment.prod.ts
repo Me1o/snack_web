f6a3539dbd4b065bcaaabfa13f3c5c7ccf5e05e9
Hosam nasr <106914826+me1oZid@users.noreply.github.com>
2025-05-29 23:48:51 +0300

    env

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:3000',
-  webUrl: 'http://localhost:4200',
+  production: true,
+  baseUrl: 'https://snack-api.onrender.com',
 };

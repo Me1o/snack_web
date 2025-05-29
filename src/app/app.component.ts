904352ff6f7b6e4cf0cb6c03746a03af0ce08de8
Hosam nasr <106914826+me1oZid@users.noreply.github.com>
2025-05-29 18:55:55 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DateFnsModule } from 'ngx-date-fns';
+import { AngularDraggableModule } from 'angular2-draggable';
+import { CommonModule } from '@angular/common';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    DateFnsModule,
+    AngularDraggableModule,
+    CommonModule,
+    TimelineComponent,
+    ToolbarComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'digitalBackyard';
+  title = 'snackWeb';
 }
